e63f5467eb1e0edec5894e91d39db8c398970183
Hendy Nur Sholeh <102641376+HendyNurSholeh@users.noreply.github.com>
2024-11-27 15:40:07 +0800

    Singleton: test singleton

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Data\Bar;
 use App\Data\Foo;
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,5 +16,34 @@ class ServiceContainerTest extends TestCase
         $bar = $this->app->make(Bar::class);
         $this->assertEquals("foo", $foo->foo()); 
         $this->assertEquals("foo and Bar", $bar->bar()); 
+    }
+
+    public function testMakeObjectNotSame(): void{
+        $foo1 = $this->app->make(Foo:: class);
+        $foo2 = $this->app->make(Foo:: class);
+        $this->assertNotSame($foo1, $foo2);
+    }
+
+    public function testBind(){
+        $this->app->bind(Person::class, function ($app) {
+            return new Person('John', 'Doe');
+        });
+        
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+        $this->assertNotNull($person1);
+        $this->assertNotNull($person2);
+        $this->assertNotSame($person1, $person2);
+    }
+    public function testSingleton(){
+        $this->app->singleton(Person::class, function ($app) {
+            return new Person('John', 'Doe');
+        });
+        
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+        $this->assertNotNull($person1);
+        $this->assertNotNull($person2);
+        $this->assertSame($person1, $person2);
     }
 }
